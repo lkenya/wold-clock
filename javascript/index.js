714d58412dc8af82e771500ae5be50fcab80e676
Lovienia Kamusasa <klovienia@gmail.com>
2024-12-08 02:06:38 +0200

    Added Javascript for single locations

---
+++ javascript/index.js
@@ -0,0 +1,21 @@
+function updateTime() {
+  //Los Angeles
+  let losAngelesElement = document.querySelector("#los-angeles");
+  let losAngelesDateElement = document.querySelector(".date");
+  let losAngelesTimeElement = document.querySelector(".time");
+  let losAngelesTime = moment().tz("America/Los_Angeles");
+  losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM Do YYYY");
+  losAngelesTimeElement.innerHTML = losAngelesTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+
+  //Paris
+  let parisElement = document.querySelector("#paris");
+  let parisDateElement = document.querySelector("#paris .date");
+  let parisTimeElement = document.querySelector("#paris .time");
+  let parisTime = moment().tz("Europe/Paris");
+  parisDateElement.innerHTML = parisTime.format("MMMM Do YYYY");
+  parisTimeElement.innerHTML = parisTime.format("h:mm:ss [<small>]A[</small>]");
+}
+updateTime();
+setInterval(updateTime, 1000);
